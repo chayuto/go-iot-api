8864be555d8d39e598064dac95deb3d183992f17
Chayut Orapinpatipat <chayut_o@hotmail.com>
2022-09-10 20:34:38 +1000

    update 404
    update 404

--- main.go
+++ main.go
@@ -15,5 +15,9 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
